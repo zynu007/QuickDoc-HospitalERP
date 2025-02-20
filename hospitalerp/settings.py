2b4f4cac20b301b71efab1709b3dee11fa08d310
zynu007 <130402243+zynu007@users.noreply.github.com>
2025-02-20 09:39:06 +0530

    final

--- hospitalerp/settings.py
+++ hospitalerp/settings.py
@@ -12,14 +12,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url 
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-run03!j!(c8uysdwrsuk+z_yvm2s&h+v==37xqe@v=_k91+t=q'
 
